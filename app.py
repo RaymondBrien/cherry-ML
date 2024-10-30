2f7c1086482438686a49d1d21fac6f6fd01672fb
Raymond Brien <raytbrien@gmail.com>
2024-10-30 15:22:10 +0000

    Add app pages outline for st structure

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 # load pages scripts
-# TODO add app pages see walkthrough1 eg from app_pages.page_summary import page_summary_body
+from app_pages.page_summary import page_summary_body
+from app_pages.page_leaf_visualiser import page_leaf_visualizer_body
+from app_pages.page_infection_detector import page_mildew_detector_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_ml_performance_metrics import page_ml_performance_metrics
 
-app = MultiPage(app_name="Mildew Detector")  # Create an instance of the app
 
-# Add your app pages here using .add_page()
+app = MultiPage(app_name="Mildew Detector")  # Create app instance
+
 # TODO add correct page names per my readme
-app.add_page("Quick Project Summary", page_summary_body)
-app.add_page("Cells Visualiser", page_cells_visualizer_body)
-app.add_page("Malaria Detection", page_malaria_detector_body)
+app.add_page("Brief Project Summary", page_summary_body)
+app.add_page("Leaf Visualiser", page_leaf_visualizer_body)
+app.add_page("Mildew Detection", page_mildew_detector_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
 app.add_page("ML Performance Metrics", page_ml_performance_metrics)
 
-app.run()  # Run the app
+app.run()
