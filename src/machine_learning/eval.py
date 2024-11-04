bc6c3eca8be2da055cc6316050e51d01a3750a01
Raymond <raytbrien@gmail.com>
2024-11-04 13:26:28 +0000

    Specify test eval for analysis

--- src/machine_learning/eval.py
+++ src/machine_learning/eval.py
@@ -2,8 +2,8 @@ import streamlit as st
 from src.data_management import load_pkl_file
 
 
-def load_test_evaluation(version):  #TODO add val and train too?
+def load_test_evaluation(version):  #TODO add val and train too for sake of seeing whole picture if more info required
     """
     This function loads the test evaluation output or results
     """
-    return load_pkl_file(f'outputs/{version}/evaluation.pkl')
+    return load_pkl_file(f'outputs/{version}/test-evaluation.pkl')
