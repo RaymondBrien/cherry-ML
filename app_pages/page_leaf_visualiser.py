1df6b5c2030abee3a6f1f3af263a11d02bbcb95a
Raymond <raytbrien@gmail.com>
2024-11-04 15:16:04 +0000

    Add textual information for page use

--- app_pages/page_leaf_visualiser.py
+++ app_pages/page_leaf_visualiser.py
@@ -1,32 +1,85 @@
-import os
-import itertools
-import random
+import os, time, itertools, random
 import streamlit as st
-import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 from matplotlib.image import imread
 
 
 def page_leaf_visualiser_body():
-    """ Create page body for leaf visualisation image montages"""
+    """
+    Create page body for leaf visualisation image montages
 
+    - Mean average image per class
+    - Difference between average healthy and average infected
+    - Image montage of raw dataset
+
+    """
+
+    # Define model for output images source
+    version = 'v6'
+
+    # Page title
     st.header("Cherry Leaves Visualiser")
-
     st.success(
-        "As part of the dataset exploration phase, this page provides a reference "
-        "of the visual differences between **healthy** Cherry Leaves "
-        "and Cherry Leaves infected with **powdery mildew**, in answer to "
-        "Business Requirement No.1 as defined by the client.\n"
+        "As part of the dataset exploration phase, this page provides a "
+        "reference for observing the visual differences between **healthy** "
+        "Cherry Leaves and Cherry Leaves infected with **powdery mildew** "
+        "in answer to Business Requirement No.1 as defined by the client.\n"
     )
-    version = 'v6'  # TODO confirm version
-    if st.checkbox("Differences between average and variability images"):
+
+    # Show the image montage
+    if st.checkbox("Image Montage"):
+        # Show a spinner during a process
+        with st.spinner(text="In progress"):
+            time.sleep(3)
+            st.success("Done")
+        st.write(
+            "* Click the 'Create Montage' button for an image montage of "
+            "healthy or infected leaves\n")
+        st.info(
+            "* This montage is a great way to visualise the raw collection of "
+            "images used in this project to better undersand how each label's "
+            "image collection is representative of its respective label.\n"
+
+            "In short: we can see a *clear visual distinction* between healthy "
+            "and infected leaves: \n"
+            "* Healthy leaves appear green and uniform in color.\n"
+            "* Infected leaves display a clear pattern of white powdery mildew "
+            "across the surface, usually as white spots or patches.\n"
+        )
+
+        my_data_dir = 'inputs/cherryleaves_dataset/cherry-leaves'
+        # TODO correct labels path - images should be from inputs
+        labels = os.listdir(my_data_dir + '/validation')
+        label_to_display = st.selectbox(
+            label="Select label:", options=labels, index=0)
+        if st.button("Create Montage"):
+            image_montage(dir_path=my_data_dir + '/test',
+                          label_to_display=label_to_display,
+                          nrows=8, ncols=3, figsize=(10, 25))
+        st.write("---")
+
+    # show mean average image per class
+    if st.checkbox("Show average image per class"):
 
         avg_var_healty = plt.imread(f"outputs/{version}/avg_var_healthy.png")
-        avg_var_powdery_mildew = plt.imread(
-            f"outputs/{version}/avg_var_powdery_mildew.png")
+        avg_var_powdery_mildew = plt.imread(f"outputs/{version}/avg_var_powdery_mildew.png")
+
         st.success(
+            # TODO edit line breaks for whole page
+            # TODO decide where to put the text below: for more technical audience
+        ## Conclusions:
+        # - Whilst to the human eye, the mean average image per label appears similar,
+        # the distinction between individual images of each class are clearly visible from the
+        # montage: given the distinct color differences between healthy and infected leaves,
+        # this should not be difficult for a CNN to learn and is an appropriate problem for
+        # which a CNN will be able to provide a robust solution according to the required
+        # accuracy metrics defined by the client's business requirements.
+        # - The image dataset is overall very small for a CNN. Whilst developing the model,
+        # overfitting will be likely and Image augmentation will be neccessary for effective
+        # learning.
+
             f" A significant visual difference in consistent colouring has been \n"
             f" observed: \n"
             f" We notice that the average and variability images of infected leaves \n"
@@ -35,59 +88,58 @@ def page_leaf_visualiser_body():
             f" However the average and variability images did not show any clear patterns \n"
             f" that could intuitively differentiate infected leaves and healthy leaves."
         )
-        st.image(avg_var_healty, caption='Healty leaves - Avegare and Variability')
-        st.image(avg_var_powdery_mildew,
-                 caption='Powdery Mildew infected leaves - Average and Variability')
+
+        # TODO try with larger number (more than 100) - perhaps whole dataset?
+        st.image(avg_var_healty, caption=f'Healthy Leaf **(mean average of dataset)**')
+        st.image(avg_var_powdery_mildew, caption='Infected Leaf **(mean average of dataset)**')
+
         st.write("---")
-    # Show the difference between average and variability images
-    if st.checkbox("Differences between average healthy and average infected leaves"):
+
+    # show difference image between average classes
+    if st.checkbox("Difference between average healthy and average infected leaves"):
         diff_between_avgs = plt.imread(f"outputs/{version}/avg_diff.png")
+
+        # TODO try other way around: x-y but also y-x to see if there are any insights?
         st.success(
-            f"* We appreciate similar pattern here, where the healthy leaves have a "
-            f"more clear, green surface, and the infected ones have more white color in "
-            f"the surface. But again it is difficult to compare both "
-            f" kind of leaves."
+            "Whilst we can see from the image montage that there are, more "
+            "often than not, clear visual differences between individual healthy "
+            "and infected leaves, this difference image does not clearly show "
+            "the visible differences so easily.\n"
+
+            "It is worth noting that the leaf perimter outline is still overall present.\n"
+            "This could be due to the fact that the average images are not "
+            "representative of the entire dataset but a mean average.\n"
+            "WIth this in mind, it is clear that further technical handling of the "
+            "sets of images will be required to draw effective machine learning "
+            "performance.\n"
         )
 
         st.image(diff_between_avgs, caption='Difference between average images')
 
-    # Show the image montage
-    if st.checkbox("Image Montage"):
-        st.write(
-            "* To create and refresh the montage, click on 'Create Montage' button")
-        st.info(
-            f"* The montage helps to visually differentiate between a healthy"
-            f" leaf and an infected one. The infected one has white, powdery "
-            f"spots or patches on the top side of leaves"
-        )
-        my_data_dir = 'inputs/cherryleaves_dataset/cherry-leaves'
-        labels = os.listdir(my_data_dir + '/validation')
-        label_to_display = st.selectbox(
-            label="Select label:", options=labels, index=0)
-        if st.button("Create Montage"):
-            image_montage(dir_path=my_data_dir + '/validation',
-                          label_to_display=label_to_display,
-                          nrows=8, ncols=3, figsize=(10, 25))
-        st.write("---")
+    
 
 
 def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15, 10)):
     """
-
-
+    Generate image montage of specififed label
+    Code taken directly from this repository's Jupyter Notebook:
+    DataVisualisation.ipynb
     """
     sns.set_style("dark")
     labels = os.listdir(dir_path)
 
     # checks if your montage space is greater than subset size
-    images_list = os.listdir(dir_path+'/' + label_to_display)
+    images_list = os.listdir(dir_path+ '/' + label_to_display)
     if nrows * ncols < len(images_list):
         img_idx = random.sample(images_list, nrows * ncols)
     else:
-        print(
-            f"Decrease nrows or ncols to create your montage. \n"
-            f"There are {len(images_list)} in your subset. "
-            f"You requested a montage with {nrows * ncols} spaces")
+        st.warning(
+            # TODO add warning icon
+            "Decrease the number of *rows* or *columns* to"
+            "create your montage.\n"
+            f"There are {len(images_list)} in your subset.\n"
+            f"You requested a montage with {int(nrows)} rows "
+            f"and {int(ncols)} columns, making {nrows * ncols} spaces")
         return
 
     # create list of axes indices based on nrows and ncols
@@ -108,7 +160,3 @@ def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15, 10)):
     plt.tight_layout()
 
     st.pyplot(fig=fig)
-
-
-    # Display image montage of label_to_display,
-    # selected from checkbox.
