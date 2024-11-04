d13daa08c1d10ade831075f1017327f62553837b
Raymond <raytbrien@gmail.com>
2024-11-04 10:50:44 +0000

    Enable eval pkl access for dashbaord

---
+++ src/machine_learning/eval.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from src.data_management import load_pkl_file
+
+
+def load_test_evaluation(version):  #TODO add val and train too?
+    """
+    This function loads the test evaluation output or results
+    """
+    return load_pkl_file(f'outputs/{version}/evaluation.pkl')
