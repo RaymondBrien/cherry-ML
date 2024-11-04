299e80ebb8eaf680b72624c41bb22439482a90af
Raymond <raytbrien@gmail.com>
2024-11-04 10:48:06 +0000

    Handle data access for dashboard

---
+++ src/data_management.py
@@ -0,0 +1,27 @@
+import os
+import base64
+import joblib
+
+import numpy as np
+import pandas as pd
+
+from datetime import datetime
+
+def download_dataframe_as_csv(df):
+    """
+    Download specified dataframe in csv format
+
+    For use in dashbaord page: ___ TODO define which page
+    """
+
+    datetime_now = datetime.now().strftime("%d%b%Y_%Hh%Mmin%Ss")
+    csv = df.to_csv().encode()
+    b64 = base64.b64encode(csv).decode()
+    href = (
+        f'<a href="data:file/csv;base64,{b64}" download="Report {datetime_now}.csv" '
+        f'target="_blank">Download Report</a>'
+    )
+    return href
+
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
