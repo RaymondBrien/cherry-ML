6ca1c0c3f77c142a667d4bc6864181c8e487de5f
Raymond <raytbrien@gmail.com>
2024-11-04 15:46:20 +0000

    Correct typos for functionality

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.page_leaf_visualiser import page_leaf_visualiser_body
 from app_pages.page_mildew_detector import page_mildew_detector_body
-from app_pages.page_project_hypothesis import page_project_hypothesis_body
-from app_pages.page_ml_performance_metrics import page_ml_performance_metrics_body
+from app_pages.page_project_hypothesis import project_hypothesis_page_body
+from app_pages.page_ml_performance_metrics import page_ml_performance_metrics
 
 
 app = MultiPage(app_name="Mildew Detector")  # Create app instance
@@ -15,7 +15,7 @@ app = MultiPage(app_name="Mildew Detector")  # Create app instance
 app.add_page("Brief Project Summary", page_summary_body)
 app.add_page("Leaf Visualiser", page_leaf_visualiser_body)
 app.add_page("Mildew Detection", page_mildew_detector_body)
-app.add_page("Project Hypothesis", page_project_hypothesis_body)
-app.add_page("ML Performance Metrics", page_ml_performance_metrics_body)
+app.add_page("Project Hypothesis", project_hypothesis_page_body)
+app.add_page("ML Performance Metrics", page_ml_performance_metrics)
 
 app.run()
