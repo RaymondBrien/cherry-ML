b9bbae376286fd26c04c89f4528ecdce50419009
Raymond Brien <raytbrien@gmail.com>
2024-11-05 13:34:49 +0000

    Enable multiple pkl evals for dashboard

--- src/machine_learning/eval.py
+++ src/machine_learning/eval.py
@@ -1,9 +1,27 @@
 import streamlit as st
+
+from typing import Union, List, Literal
 from src.data_management import load_pkl_file
 
 
-def load_test_evaluation(version):  #TODO add val and train too for sake of seeing whole picture if more info required
+def load_evaluation(version: int, dataset: Union[Literal['train', 'validation', 'test'], List[str]]):
     """
-    This function loads the test evaluation output or results
+    Dynamically load the evaluation pickle file for specified 
+    Return a dataframe
+    
+    Args:
+        - version: specify model version for correct output folder access
+        - dataset: specify which evaluation pkl
     """
-    return load_pkl_file(f'outputs/{version}/test-evaluation.pkl')
+    # if a single dataset is provided as a string, convert it to a list for consistency
+    if isinstance(dataset, str):
+        dataset = [dataset]
+    
+    # dictionary to store each dataset's evaluation
+    evaluations = {}
+    
+    for ds in dataset:
+        evaluations[ds] = load_pkl_file(f'outputs/{version}/{ds}-evaluation.pkl')
+    
+    return evaluations
+
