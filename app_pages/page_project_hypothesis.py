7c11ae0e7b6743a5b9bd0344342722fe08917105
Raymond Brien <raytbrien@gmail.com>
2024-11-06 16:15:46 +0000

    Add validation 3 to match readme

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -5,35 +5,31 @@ def project_hypothesis_page_body():
     """Displays project hypothesis and validation page content."""
 
     st.header("Project Hypothesis and Validation")
+    st.info('**Hypothesis 1**')
+    st.markdown("""
+    - **Hypothesis 1**: It is hypothesized that infected cherry tree leaves will be visually differentiable from healthy cherry tree leaves. Specifically, white powdery mildew will be present on the surface of
+    the majority of infected leaves within the dataset.
+    - **Validation**:
+        - **Image Montage**: To visually illustrate the differences between healthy and infected leaves.
+        - **Average Image Per Class**: Calculating the average image for both healthy and infected leaves to identify any distinct color or texture patterns.
+        - **Difference Between Averages**: Comparing the average images of healthy and infected leaves to highlight visual differences.
+    """)
 
-    st.info(
+    st.info('**Hypothesis 2**')
+    st.markdown("""
+        - **Hypotheesis 2**: It is hypothesised that using only the provided dataset, the ML model will be able to distinguish between a healthy cherry leaf and an infected cherry leaf with at least 97% accuracy.
+        - **Validation**:
+            - **Accuracy Evaluation on Test Set**: After model training, model evaluation will measure accuracy and loss readings. Accuracy above 97% per business requirement 2, will pass. This is ultimately the main business driver.
+            - **Model Output**: Accuracy 99.88% (*Pass True*)
+    """)
 
-        "Hypothesis 1: It is hypothesized that infected cherry tree leaves will be visually "
-        "differentiable from healthy cherry tree leaves. Specifically, white powdery "
-        "mildew will be present on the surface of the majority of infected leaves "
-        "within the dataset.\n"
+    st.info('**Hypothesis 3**')
+    st.markdown("""
+        - **Hypothesis 3 (technical)**: It is hypothesised that at least 20 epochs will be required for effective generalisation of the data from the given dataset.
+        - **Validation**: Epoch count during training once model has been fitted
+            - **Model Output**: From early stopping, only 11 epochs were needed for suitable accuracy results.
+    """)
 
-        # TODO update to correct accuracy metric
-        "* Validation 1: Based on the data visualization notebook, infected leaves displayed "
-        "white powdery mildew on their surfaces across multiple image montages, "
-        "making them visually distinct from healthy leaves. Although generating a "
-        "mathematical difference between average images of each class did not "
-        "reveal strongly visible pattern differences to the human eye, the "
-        "the class variability images showed sufficient shape contrasts within the raw "
-        "dataset to train a CNN successfully.\n"
-        "Furthermore, the trained ML model achieves an accuracy of x% in distinguishing "
-        "between the two classes, providing a quantitative metric to support "
-        "this hypothesis.\n"
 
-        "Hypothesis 2: It is hypothesised that using only the provided dataset, the ML model will "
-        "be able to distinguish between a healthy cherry leaf and an infected cherry leaf "
-        "with at least 97% accuracy."
-        # TODO update to correct accuracy metric
-        "* Validation 2: The model can able to distinguish between image classes correctly with "
-        "x% accuracy, certified by the evaluation metrics"
 
-        "Hypothesis 3: Augmenting the training dataset with transformations (like rotation, zoom, "
-        "or contrast changes) will improve the model's performance."
-        # TODO validte this with images
-        "* Validation 3: "
-            )
+
